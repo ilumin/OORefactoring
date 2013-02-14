8fcc3c7f40ab16182831e088cfee127cbe554925
ilumin <iolumin@gmail.com>
2013-02-14 23:10:33 +0700

    Find some bad smell code
    Hmmmm, smell bad huh?

--- src/StringCalculator/StringCalculator.java
+++ src/StringCalculator/StringCalculator.java
@@ -5,14 +5,23 @@ public class StringCalculator {
         if (s.isEmpty()) {
             return 0;
         }
+        
+        // to complicate to change string
         if (s.startsWith("//")) {
             int returnValue = 0;
+            
+            // too complicate to change string
+            // look like it duplicate code ?
+            // long parameter ?
             for (String n : s.substring(4).split("[\n"+ s.charAt(2) +"]")) {
                 returnValue += Integer.parseInt(n);
             }
             return returnValue;
         } else {
             int r = 0; // result
+            
+            // too complicate to change string
+            // look like it duplicate code
             for (String n : s.split("[\n,]")) {
                 r += Integer.parseInt(n);
             }
