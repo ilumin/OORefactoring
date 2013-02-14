7153fbdba1c83e1325f753abeb849ebdab0eab28
ilumin <iolumin@gmail.com>
2013-02-14 23:52:33 +0700

    Extract constant
    move variable to constant
    those variable will be easy to change

--- src/StringCalculator/StringCalculator.java
+++ src/StringCalculator/StringCalculator.java
@@ -1,29 +1,27 @@
 package StringCalculator;
 
 public class StringCalculator {
+
+	// New constant
+	public static final char STANDARD_SEPARATOR = ',';
+	public static final String DEFINE_CUSTOM_SEPARATOR = "//";
+	
     public int add(String inputString) {
         if (inputString.isEmpty()) {
             return 0;
         }
         
-        // to complicate to change string
-        if (inputString.startsWith("//")) {
+        if (inputString.startsWith(DEFINE_CUSTOM_SEPARATOR)) {
             char customSeparator = inputString.charAt(2);
             
-            // Extract Local Variable (to use in new Extract Method)
             String stringToCalculate = inputString.substring(4);
             
-            // Call that new Extract Method
             return add(stringToCalculate, customSeparator);
         } else {
-            char customSeparator = ',';
-            
-            // Call that new Extract Method
-            return add(inputString, customSeparator);
+            return add(inputString, STANDARD_SEPARATOR);
         }
     }
 
-    // New method here
 	private int add(String s, char customSeparator) {
 		int result = 0;
 		
