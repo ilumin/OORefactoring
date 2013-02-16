2a57cfe70743a57cb191c4b6a51cc25591d8c41d
Teerasak Vichadee <iolumin@gmail.com>
2013-02-16 12:13:46 +0700

    Update test case
    one test function per case

--- src/StringCalculator/StringCalculatorTest.java
+++ src/StringCalculator/StringCalculatorTest.java
@@ -1,30 +1,36 @@
 package StringCalculator;
 
 import static org.junit.Assert.*;
-import StringCalculator.StringCalculator;
 
 import org.junit.Test;
 
 public class StringCalculatorTest {
+	
+	StringCalculator stringCalculator = new StringCalculator();
 
 	@Test
-	public void testCalculator() throws Exception {
-		StringCalculator s = new StringCalculator(); // define target class
-		
-		System.out.println("Input: '', Output: 0 (Should treat empty string as zero)");
-		assertEquals(0, s.add("")); 
-		
-		System.out.println("Input: '17', Output: 17 (Should add single numbers)");
-		assertEquals(17, s.add("17")); 
-		
-		System.out.println("Input: '2341,15', Output: 2356 (Should add comma separated numbers)");
-		assertEquals(2356, s.add("2341,15"));
-		
-		System.out.println("Input: '34\\n1', Output: 35 (Should treat new line as separator)");
-		assertEquals(35, s.add("34\n1")); 
-		
-		System.out.println("Input: '//;\\n5;2', Output: 7 (Should support custom separators)");
-		assertEquals(7, s.add("//;\n5;2")); 
+	public void testEmptyString(){
+        assertEquals(0, stringCalculator.add("")); 
+	}
+
+	@Test
+	public void testSingleNumber(){
+		assertEquals(17, stringCalculator.add("17"));
+	}
+
+	@Test
+	public void testCommaSeparateNumber(){
+		assertEquals(2356, stringCalculator.add("2341,15"));
+	}
+
+	@Test
+	public void testNewlineSeparateNumber(){
+		assertEquals(35, stringCalculator.add("34\n1"));
+	}
+
+	@Test
+	public void testCustomSeparator(){
+		assertEquals(7, stringCalculator.add("//;\n5;2"));
 	}
 
 }
